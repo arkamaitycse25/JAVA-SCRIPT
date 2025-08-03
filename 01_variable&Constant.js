9242d083c5b858c444d182f78ee8f6a67d5f41a3
arka maity <167577867+arkamaitycse25@users.noreply.github.com>
2025-08-03 14:53:49 +0530

    Add files via upload

---
+++ 01_variable&Constant.js
@@ -0,0 +1,12 @@
+const accountId=12345;
+let accountEmail="Arka.csc01@gmail.com";
+var accountPassword="123";
+accountCity="kolkata";
+let accountState;
+
+// accountId=11432;
+accountEmail="sulekha.csc01@gmail.com";
+accountPassword="321";
+accountCity="Delhi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity.accountState])
---
+++ 02_datatype.js
@@ -0,0 +1,24 @@
+let name="Arka"
+let age=25
+let isWinter=false
+let result
+let number=12345678942686352655
+let value=null
+
+console.log(name);
+console.log(typeof name)
+
+console.log(age);
+console.log(typeof age)
+
+console.log(isWinter);
+console.log(typeof isWinter)
+
+console.log(result);
+console.log(typeof result)
+
+console.log(number);
+console.log(typeof number)
+
+console.log(value);
+console.log(typeof value)
