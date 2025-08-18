fe6f58ebc972be2d52582482ec44df1cd4c710c0
arka maity <167577867+arkamaitycse25@users.noreply.github.com>
2025-08-19 01:33:42 +0530

    Add files via upload

---
+++ 07_string.js
@@ -0,0 +1,32 @@
+// string
+
+let str="Arka Maity";
+console.log(str);
+console.log(str.length);
+console.log(str[2]);
+
+// Template Literals
+
+let specialString=`This is a Template Literal`;
+console.log(specialString);
+
+let obj={
+    item:"pen",
+    price:10,
+};
+console.log(`The cost of ${obj.item} is ${obj.price}`); 
+
+// Escape character --> /n(fot next line),/t(for tab space)
+
+console.log("Arka/nMaity");
+console.log("Arka/tMaity");
+
+// String Method
+
+let str1="ARKA MAITY";
+let str2="sulekha maity";
+console.log(str2);
+console.log(str2.toUpperCase());
+console.log(str1)
+console.log(str1.toLowerCase());
+
